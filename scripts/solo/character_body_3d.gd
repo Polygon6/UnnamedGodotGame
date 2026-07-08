3651c951fc0511014affbb2389d390d8383df3a8
Polygon6 <kacperkillarney@gmail.com>
2026-07-08 22:01:44 +0100

    so theres movement now

--- scripts/solo/character_body_3d.gd
+++ scripts/solo/character_body_3d.gd
@@ -1,9 +1,20 @@
 extends CharacterBody3D
 
-
 const SPEED = 2.0
 const JUMP_VELOCITY = 2.5
+const s = 0.02
 
+@onready var head = $Node3D
+@onready var cam = $Node3D/Camera3D
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x*s)
+		cam.rotate_x(-event.relative.y*s)
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,8 +27,8 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
