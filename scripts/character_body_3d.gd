d118f88005acf662a39b012956ac0470c99b3b34
Polygon6 <kacperkillarney@gmail.com>
2026-07-11 00:19:45 +0100

    made movement 1% less bad

--- scripts/character_body_3d.gd
+++ scripts/character_body_3d.gd
@@ -1,11 +1,12 @@
 extends CharacterBody3D
 
-const SPEED = 2.0
-const JUMP_VELOCITY = 2.5
+const g = Vector3(0, -24, 0)
+const speed = 9
+const jUp = 15
 const s = 0.02
 
-@onready var head = $Node3D
-@onready var cam = $Node3D/Camera3D
+@onready var cam = $head/Camera3D
+@onready var head = $head
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseMotion:
@@ -19,21 +20,21 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += g * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jUp
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
